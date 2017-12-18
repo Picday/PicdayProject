8e7c882b04cc4872bd971ab3aaed974dfffa2f4f
berta.domingo <bertadomingo1@hotmail.com>
2017-12-18 16:04:54 +0100

    PicDay amb menu

--- app/src/main/java/com/example/bertadomingo/picday/PicdayActivity.java
+++ app/src/main/java/com/example/bertadomingo/picday/PicdayActivity.java
@@ -1,7 +1,11 @@
 package com.example.bertadomingo.picday;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.GridView;
 
 public class PicdayActivity extends AppCompatActivity{
@@ -16,5 +20,23 @@ public class PicdayActivity extends AppCompatActivity{
         gridView = (GridView) findViewById(R.id.id_gridview);
         adaptador = new GridAdapter(this);
         gridView.setAdapter(adaptador);
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menupicday, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        /*if(id == R.id.videomaker){
+            // lo ideal aquí sería hacer un intent para abrir una nueva clase como lo siguiente
+            Log.i("ActionBar", "Settings!");
+            Intent about = new Intent(getApplicationContext(), About.class);
+            startActivity(about);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);*/
+        return true;
     }
 }
